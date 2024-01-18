2652dea57aa8c48c3d56420eaa782e660bf7e841
Italo Travenzoli <italotravenzoli@gmail.com>
2024-01-18 10:12:46 -0300

    Covid 19
    Upload da pasta Covid 19

---
+++ Covid_19/Assets/scripts/setRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setRotation : MonoBehaviour
+{
+    public GameObject palavra;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "cubo")
+            //transform.Rotate(new Vector3(0.0f, 0.0f, 0.0f));
+            palavra.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+}
---
+++ Covid_19/Assets/scripts/subir.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class subir : MonoBehaviour
+{
+    public GameObject mutacao;
+    private float countdown = 0.5f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+
+        if (Input.anyKeyDown)
+        {
+            transform.Rotate(new Vector3(Random.Range(10, 60), Random.Range(10, 60), Random.Range(10, 60)));
+            mutacao.SetActive(true);
+
+            countdown -= Time.deltaTime;
+            if (countdown <= 0.0f)// zera contador e mostra palavra Mutacao
+            {
+                countdown = 0.5f;
+                mutacao.SetActive(false);
+
+            }
+
+        }
+        else
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0.0f)// zera contador e esconde palavra mutacao
+            {
+                countdown = 0.5f;
+                mutacao.SetActive(false);
+                
+            }
+        }
+
+    }
+
+
+}
